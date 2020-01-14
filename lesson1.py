b6c200f9089f4208d3a2597635852b10eb631e32
Iryna <iryna.sherepot@gmail.com>
2020-01-13 19:30:55 -0800

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,32 @@
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create dict comprehension: new_fellowship
+new_fellowship = {member: len(member) for member in fellowship}
+
+# Print the new dictionary
+print(new_fellowship)
+
+
+# Comprehentions - store list in memory
+#Generators - return it when needed. Gen Object does not include the entire list.
+# Use yield instead of print or return
+
+# Create generator object: result
+result = (num for num in range(31))
+
+# Print the first 5 values
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+
+# Print the rest of the values
+for value in result:
+    print(value)
+
+
+
+
+
